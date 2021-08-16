0a0bfabbd6651ae26538eda8c3d3201c108a66b6
John Stanco <stancoj9@gmail.com>
2021-08-16 00:54:25 -0400

    Add dumps and loads methods for serializing directly to/from bytes

--- radarqc/csfile.py
+++ radarqc/csfile.py
@@ -1,4 +1,6 @@
-from typing import BinaryIO
+import io
+
+from typing import BinaryIO, Optional
 
 import numpy as np
 
@@ -57,14 +59,46 @@ class CSFile:
         return self._spectrum.cross23
 
 
-def load(f: BinaryIO, preprocess: SignalProcessor = None) -> CSFile:
+def load(
+    file: BinaryIO, preprocess: Optional[SignalProcessor] = None
+) -> CSFile:
+    """Deserialize file (a .read()-supporting binary file-like object
+    containing cross-spectrum file data) to a Python object.
+
+    preprocess is an optional SignalProcessor object that will be called with
+    the result of the deserialization. The return value of preprocess will be
+    used instead of the original object. This feature can be used to implement
+    custom preprocessing or filtering of spectrum data."""
     if preprocess is None:
         preprocess = Identity()
 
-    header, spectrum = CSFileReader().load(f, preprocess)
+    header, spectrum = CSFileReader().load(file, preprocess)
     return CSFile(header, spectrum)
 
 
-def dump(cs: CSFile, f: BinaryIO) -> None:
-    header, spectrum = cs.header, cs.spectrum
-    CSFileWriter().dump(header, spectrum, f)
+def loads(
+    archive: bytes, preprocess: Optional[SignalProcessor] = None
+) -> CSFile:
+    """Deserialize archive (bytes instance containing cross-spectrum file data)
+    to a Python object.
+
+    preprocess is an optional SignalProcessor object that will be called with
+    the result of the deserialization. The return value of preprocess will be
+    used instead of the original object. This feature can be used to implement
+    custom preprocessing or filtering of spectrum data."""
+    return load(io.BytesIO(archive), preprocess)
+
+
+def dump(obj: CSFile, file: BinaryIO) -> None:
+    """
+    Serialize obj as a cross-spectra binary archive to file (a
+    .write()-supporting binary file-like object)."""
+    header, spectrum = obj.header, obj.spectrum
+    CSFileWriter().dump(header, spectrum, file)
+
+
+def dumps(obj: CSFile) -> bytes:
+    """Serialize obj to a cross-spectra binary archive"""
+    archive = io.BytesIO()
+    dump(obj, archive)
+    return archive.getbuffer()
